6da345ad8cf0f626e5814794d23c9e233b4e1161
Sepako <sepako@o2.pl>
2022-03-12 07:50:52 +0100

    ok

---
+++ cmd_git.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+#git pull # update data
+#cp ../../cpp/temper.png .
+#cp ../../cpp/temper.csv .
+
+#cp /home/pi/data/volta.dat .
+#cp /home/pi/data/watta.dat .
+git commit -a -m 'ok'
+git push -u origin main
+#git add [file]
+#git rm -f [file]
